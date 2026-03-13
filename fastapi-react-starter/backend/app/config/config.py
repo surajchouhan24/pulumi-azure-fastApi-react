4c434865aa73e7652f148652fb153b065e09954e
surajchouhan24 <suraj.chouhan@gemsessence@gmail.com>
2026-03-13 17:08:37 +0530

    fix cors env parsing

--- fastapi-react-starter/backend/app/config/config.py
+++ fastapi-react-starter/backend/app/config/config.py
@@ -28,17 +28,35 @@ class Settings(BaseSettings):
     #     if isinstance(v, str):
     #         return [i.strip() for i in v.split(",")]
     #     return v
+    # @field_validator("CORS_ORIGINS", mode="before")
+    # def assemble_cors_origins(cls, v):
+    #     if not v:
+    #         return []
+
+    #     if isinstance(v, str):
+    #         if v.startswith("["):
+    #             import json
+
+    #             return json.loads(v)
+    #         return [i.strip() for i in v.split(",")]
+
+    #     return v
+    
     @field_validator("CORS_ORIGINS", mode="before")
     def assemble_cors_origins(cls, v):
         if not v:
             return []
 
         if isinstance(v, str):
-            if v.startswith("["):
-                import json
+            v = v.strip()
 
-                return json.loads(v)
-            return [i.strip() for i in v.split(",")]
+            if v.startswith("["):
+                try:
+                    return json.loads(v)
+                except Exception:
+                    return []
+
+            return [i.strip() for i in v.split(",") if i.strip()]
 
         return v
 
